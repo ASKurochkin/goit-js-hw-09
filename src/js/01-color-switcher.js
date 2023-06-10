1cdca42aef90ffc9234573f9c2e519a19a698e85
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-06-10 09:00:27 +0300

    Done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let clrInterval;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+startBtn.addEventListener('click', ()=>{
+    clrInterval = setInterval(()=>{
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBtn.disabled = true;
+});
+
+
+stopBtn.addEventListener('click', ()=>{
+    clearInterval(clrInterval);
+    bodyEl.style.backgroundColor = `#FFFFFF`;
+    startBtn.disabled = false;
+    
+})
